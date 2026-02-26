c907b7801219eb80ed2746d143a4d047b870da2b
Jean Damascene Iranzi <iranzijeand20@gmail.com>
2026-02-26 13:42:47 +0200

    Adding loop counter

---
+++ src/test/java/exercise/Test.java
@@ -0,0 +1,13 @@
+package exercise;
+
+public class Test {
+
+    public static void main(String[] args) {
+        int count = 0;
+
+        for (int i = 1; i <= 10; i++) {
+            count++;
+            System.out.println("Total count: " + count);
+        }
+    }
+}
