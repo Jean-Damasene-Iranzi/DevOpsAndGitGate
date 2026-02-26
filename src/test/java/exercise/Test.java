8948682214136ae9f6027aafbbbcc4a568609b72
Jean Damascene Iranzi <iranzijeand20@gmail.com>
2026-02-26 15:21:54 +0200

    Changes in main to simulate conflict

--- src/test/java/exercise/Test.java
+++ src/test/java/exercise/Test.java
@@ -4,11 +4,15 @@ public class Test {
 
     public static void main(String[] args) {
         int count = 0;
+        int sum = 0;
 
         for (int i = 1; i <= 10; i++) {
+            sum += i;
             count++;
             System.out.println("Total count: " + count);
             System.out.println("Next number: " + i);
+            System.out.println("Sum: " + sum);
         }
+        System.out.println("Final Total count: " + count);
     }
 }
